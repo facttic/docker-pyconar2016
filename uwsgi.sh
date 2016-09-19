a07990503607f08b833f09990b48633ec1e99797
Juan Manuel Schillaci <juan.schillaci@devecoop.com>
2016-09-18 22:53:47 -0300

    Remove user and group from gunicorn

--- uwsgi.sh
+++ uwsgi.sh
@@ -4,5 +4,4 @@ chown uwsgi /project/pinaxcon/site_media/media
 su -m uwsgi -c "python /project/manage.py migrate --noinput"
 su -m uwsgi -c "python /project/manage.py collectstatic --noinput"
 #su -m uwsgi -c "/usr/local/bin/uwsgi --socket :5000 --wsgi-file /project/pinaxcon/wsgi.py --master --processes 3 --threads 2 --chdir /project"
-gunicorn pinaxcon.wsgi --chdir=/project -b 0.0.0.0:5000 -w 8 --user=uwsgi
---group=uwsgi
+gunicorn pinaxcon.wsgi --chdir=/project -b 0.0.0.0:5000 -w 8
